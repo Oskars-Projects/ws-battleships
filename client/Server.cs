dbef36082e92e2855f1b7cad99a9f55d021f3b7e
oskar <36141470+devskar@users.noreply.github.com>
2022-11-09 14:51:37 +0100

    fixing renaming problem

--- client/Server.cs
+++ client/Server.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
+class Server
+{
+
+    public static void Main()
+    {
+        IPAddress serverIpAdress = IPAddress.Any;
+        TcpListener server = new TcpListener(serverIpAdress, 5000);
+        server.Start();
+        Console.WriteLine("Listening on: " + serverIpAdress.ToString());
+
+        server.BeginAcceptTcpClient(HandleConnection, server);
+
+    }
+
+    public static void HandleConnection(IAsyncResult res)
+    {
+        TcpClient client = 
+    }
+}
