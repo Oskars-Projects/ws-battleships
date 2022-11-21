43f20e240c00e5c3aeabfb6a163a8b35b42c0b1f
oskar <ooskar@gmx.de>
2022-11-21 17:02:24 +0100

    rewrite complete done

---
+++ server/Handlers/SocketMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using server.SocketManager;
+
+namespace server.Handlers
+{
+    public class SocketMessageHandler : SocketHandler
+    {
+        public SocketMessageHandler(ConnectionManager connections) : base(connections)
+        {
+        }
+
+        public override async Task OnConnect(WebSocket socket)
+        {
+            await base.OnConnect(socket);
+            string socketId = Connections.GetIdBySocket(socket);
+            await SendBroadcastMessage($"{socketId} just joined.");
+        }
+
+        public override async Task Receive(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            string id = Connections.GetIdBySocket(socket);
+            string message = $"{id} said: {Encoding.UTF8.GetString(buffer, 0, result.Count)}";
+            await SendBroadcastMessage(message);
+        }
+    }
+}
+
