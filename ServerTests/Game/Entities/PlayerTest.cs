d555bec35906a6605a025750904cf004eac18d65
llayla <layla.shokri@gmx.de>
2022-12-11 14:57:53 +0100

    added PlayerTest

---
+++ ServerTests/Game/Entities/PlayerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTests.Game.Entities
+{
+    [TestClass]
+    public class PlayerTest
+    {
+        [TestMethod]
+        public void HasBoatsZoPlaceTest()
+        {
+            List<int> BoatsToPlace = new List<int> {3, 3, 2, 2};
+            Assert.IsTrue(BoatsToPlace.Any());
+        }
+    }
+}
