40eaaef4d1f6575a705c7e894df484434e5e9e48
llayla <layla.shokri@gmx.de>
2022-12-20 12:11:11 +0100

    renamed directory

---
+++ ServerTests/Game/Entities/GameFieldTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server.Game.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Game.Entities.Tests
+{
+    [TestClass()]
+    public class GameFieldTests
+    {
+
+        [TestMethod()]
+        public void SetBoatLocationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
