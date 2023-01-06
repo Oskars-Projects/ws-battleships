fbfedb378ca631e43cf1f34018d231d701e930e6
oskar <ooskar@gmx.de>
2023-01-06 15:02:40 +0100

    Finished console client and server for mvp

--- Lib/GameEntities/GameField.cs
+++ Lib/GameEntities/GameField.cs
@@ -1,6 +1,7 @@
 ﻿using Lib.Constants;
 using Lib.Extensions;
 using Lib.GameEntities;
+using Newtonsoft.Json;
 
 namespace Server.Game.Entities
 {
@@ -26,15 +27,15 @@ namespace Server.Game.Entities
         /// <returns>returns true if the boat was placed successfully and false if not</returns>
         public bool AddBoat(Boat boat)
         {
-            int startX = Math.Min(boat.Start.XAsInt, boat.End.XAsInt);
-            int endX = Math.Max(boat.Start.XAsInt, boat.End.XAsInt);
+            int startX = Math.Min(boat.Start.XIdx, boat.End.XIdx);
+            int endX = Math.Max(boat.Start.XIdx, boat.End.XIdx);
 
-            int startY = Math.Min(boat.Start.Y, boat.End.Y);
-            int endY = Math.Max(boat.Start.Y, boat.End.Y);
+            int startY = Math.Min(boat.Start.YIdx, boat.End.YIdx);
+            int endY = Math.Max(boat.Start.YIdx, boat.End.YIdx);
 
-            for (int x = startX; x < endX; x++)
+            for (int x = startX; x <= endX; x++)
             {
-                for(int y = startY; y < endY; y++)
+                for(int y = startY; y <= endY; y++)
                 {
                     if ((FieldType) this[y][x] != FieldType.WATER) return false;
                     this[y][x] = (int) FieldType.BOAT;
@@ -45,7 +46,7 @@ namespace Server.Game.Entities
 
         public FieldType GetTypeFromLocation(Location location)
         {
-            return (FieldType) this[location.Y][location.XAsInt];
+            return (FieldType) this[location.YIdx][location.XIdx];
         }
 
         /// <summary>
@@ -57,15 +58,15 @@ namespace Server.Game.Entities
         {
             if (boat.Direction == Direction.HORIZONTAL)
             {
-                int startX = Math.Min(boat.Start.XAsInt, boat.End.XAsInt);
-                int endX = Math.Max(boat.Start.XAsInt, boat.End.XAsInt);
+                int startX = Math.Min(boat.Start.XIdx, boat.End.XIdx);
+                int endX = Math.Max(boat.Start.XIdx, boat.End.XIdx);
 
                 // try to remove 1 from the start if possible
                 if (startX > 0) startX--;
                 // try to add 1 to the end if possible
                 if (endX < GameFieldConstants.Size - 1) endX++;
 
-                int y = boat.Start.Y;
+                int y = boat.Start.YIdx;
 
                 for (int x = startX; x < endX; x++)
                 {
@@ -88,15 +89,15 @@ namespace Server.Game.Entities
 
             if (boat.Direction == Direction.VERTICAL)
             {
-                int startY = Math.Min(boat.Start.Y, boat.End.Y);
-                int endY = Math.Max(boat.Start.Y, boat.End.Y);
+                int startY = Math.Min(boat.Start.YIdx, boat.End.YIdx);
+                int endY = Math.Max(boat.Start.YIdx, boat.End.YIdx);
 
                 // try to remove 1 from the start if possible
                 if (startY > 0) startY--;
                 // try to add 1 to the end if possible
                 if (endY < GameFieldConstants.Size - 1) endY++;
 
-                int x = boat.Start.XAsInt;
+                int x = boat.Start.XIdx;
 
                 for (int y = startY; y < endY; y++)
                 {
@@ -121,7 +122,7 @@ namespace Server.Game.Entities
 
         public bool ContainsBoat()
         {
-            for (int y = 0; y < this.Board.Length; y++)
+            for (int y = 0; y < Board.Length; y++)
             {
                 for (int x = 0; x < this[y].Length; x++)
                 {
@@ -143,6 +144,26 @@ namespace Server.Game.Entities
                 }
                 Board[i] = temp;
             }
+        }
+
+        public int BoatPartCount()
+        {
+            int count = 0;
+
+            foreach (int[] row in Board)
+            {
+                foreach(int cell in row)
+                {
+                    if (cell == (int)FieldType.BOAT) count++;
+                }
+            }
+
+            return count;
+        }
+
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(Board);
         }
     }
 }
